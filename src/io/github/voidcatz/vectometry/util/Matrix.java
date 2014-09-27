b160bdfea77ab1e5fe3a2d09f7bede6d763d781d
Dominik Stolz <crazymrcat@gmail.com>
2014-09-27 13:20:02 +0200

    added documentation for Matrix

--- src/io/github/voidcatz/vectometry/util/Matrix.java
+++ src/io/github/voidcatz/vectometry/util/Matrix.java
@@ -7,14 +7,26 @@ import java.util.Arrays;
 import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
 
+/**
+ * uses column-major order
+ */
 public class Matrix {
 	private float[][] matrix;
 	
-	public Matrix(float[][] matrix) {
-		if(!isArrayRegular(matrix)) throw new InvalidParameterException("Given array is not regular");
-		this.matrix = matrix;
+	/**
+	 * creates a new matrix with the given values
+	 * @param values regular 2D array of float values
+	 */
+	public Matrix(float[][] values) {
+		if(!isArrayRegular(values)) throw new InvalidParameterException("Given array is not regular");
+		this.matrix = values;
 	}
 
+	/**
+	 * creates new matrix
+	 * @param n
+	 * @return identity n x n matrix
+	 */
 	public static Matrix identity(int n) {
 		Matrix mat = new Matrix(n, n);
 		for(int col = 0; col < n; col++) {
@@ -25,10 +37,21 @@ public class Matrix {
 		return mat;
 	}
 
+	/**
+	 * creates new matrix with the given numbers of columns and rows
+	 * @param columns
+	 * @param rows
+	 */
 	public Matrix(int columns, int rows) {
 		this(new float[columns][rows]);
 	}
 	
+	/**
+	 * creates a new matrix with the given values
+	 * @param columns
+	 * @param rows
+	 * @param values array of float values in column-major order
+	 */
 	public Matrix(int columns, int rows, float[] values) {
 		this(columns, rows);
 		if(this.size() != values.length) throw new InvalidParameterException("Given array does not fit into the matrix");
@@ -39,26 +62,46 @@ public class Matrix {
 		}
 	}
 
+	/**
+	 * @param col
+	 * @param row
+	 * @return the value at the given column and row
+	 */
 	public float get(int col, int row) {
 		return matrix[col][row];
 	}
 	
-	public void set(int col, int row, float value) {
+	/**
+	 * only for the construction of new matrices
+	 */
+	protected void set(int col, int row, float value) {
 		matrix[col][row] = value;
 	}
 	
+	/**
+	 * @return number of columns
+	 */
 	public int columns() {
 		return matrix.length;
 	}
 	
+	/**
+	 * @return number of rows
+	 */
 	public int rows() {
 		return matrix[0].length;
 	}
 	
+	/**
+	 * @return total number of values
+	 */
 	public int size() {
 		return this.columns() * this.rows();
 	}
 	
+	/**
+	 * @return array of all values in column-major order
+	 */
 	public float[] values() {
 		float[] values = new float[this.size()];
 		for(int col = 0; col < this.columns(); col++) {
@@ -69,34 +112,57 @@ public class Matrix {
 		return values;
 	}
 	
+	/**
+	 * @return vector whose x and y coordinates correspond to these columns and rows
+	 */
 	public Vector dimensions() {
 		return new Vector(this.columns(), this.rows());
 	}
 	
-	public boolean typeEquals(Matrix mat) {
-		return mat.dimensions().equals(this.dimensions());
+	/**
+	 * @param other matrix
+	 * @return true if these dimensions are equal to the other matrix's dimensions
+	 */
+	public boolean typeEquals(Matrix other) {
+		return other.dimensions().equals(this.dimensions());
 	}
 	
-	public Matrix add(Matrix mat) {
-		return this.operate(mat, (a, b) -> a + b);
+	/**
+	 * @param other matrix
+	 * @return new matrix whose values are the sums of these values and the other matrix's values
+	 */
+	public Matrix add(Matrix other) {
+		return this.operate(other, (a, b) -> a + b);
 	}
 	
+	/**
+	 * @param other matrix
+	 * @return new matrix whose values are the differences of these values and the other matrix's values
+	 */
 	public Matrix substract(Matrix mat) {
 		return this.operate(mat, (a, b) -> a - b);
 	}
 	
+	/**
+	 * @param other matrix
+	 * @return new matrix whose values are the scaled values of this matrix
+	 */
 	public Matrix scale(float scalar) {
 		return this.operate(a -> a * scalar);
 	}
 	
-	public Matrix multiply(Matrix mat) {
-		if(this.columns() != mat.rows()) throw new InvalidParameterException("The given matrices can't be multiplied");
-		Matrix result = new Matrix(mat.columns(  ), this.rows());
+	/**
+	 * @param other matrix whose number of rows equal this matrix's number of columns
+	 * @return new matrix with the same number of columns as the other matrix and the same number of rows as this matrix
+	 */
+	public Matrix multiply(Matrix other) {
+		if(this.columns() != other.rows()) throw new InvalidParameterException("The given matrices can't be multiplied");
+		Matrix result = new Matrix(other.columns(  ), this.rows());
 		for(int col = 0; col < result.columns(); col++) {
 			for(int row = 0; row < result.rows(); row++) {
 				float sum = 0;
 				for(int i = 0; i < this.columns(); i++) {
-					sum += mat.get(col, i) * this.get(i, row);
+					sum += other.get(col, i) * this.get(i, row);
 				}
 				result.set(col, row, sum);
 			}
@@ -104,6 +170,10 @@ public class Matrix {
 		return result;
 	}
 	
+	/**
+	 * @return the transposed matrix
+	 * -> this[x, y] = transposed[y, x]
+	 */
 	public Matrix transpose() {
 		Matrix result = new Matrix(this.rows(), this.columns());
 		for(int col = 0; col < this.columns(); col++) {
@@ -114,17 +184,27 @@ public class Matrix {
 		return result;
 	}
 	
-	public Matrix joinRight(Matrix mat) {
-		if(this.rows() != mat.rows()) throw new InvalidParameterException("The given matrices can't be joined right");
-		Matrix result = new Matrix(this.columns() + mat.columns(), this.rows());
+	/**
+	 * appends the other matrix to the right side of this matrix
+	 * @param other matrix
+	 * @return united matrix
+	 */
+	public Matrix joinRight(Matrix other) {
+		if(this.rows() != other.rows()) throw new InvalidParameterException("The given matrices can't be joined right");
+		Matrix result = new Matrix(this.columns() + other.columns(), this.rows());
 		for(int col = 0; col < result.columns(); col++) {
 			for(int row = 0; row < result.rows(); row++) {
-				result.set(col, row, col < this.columns() ? this.get(col, row) : mat.get(col - this.columns(), row));
+				result.set(col, row, col < this.columns() ? this.get(col, row) : other.get(col - this.columns(), row));
 			}
 		}
 		return result;
 	}
 	
+	/**
+	 * appends the other matrix below this matrix
+	 * @param other matrix
+	 * @return united matrix
+	 */
 	public Matrix joinBottom(Matrix mat) {
 		if(this.columns() != mat.columns()) throw new InvalidParameterException("The given matrices can't be joined right");
 		Matrix result = new Matrix(this.columns(), this.rows() + mat.rows());
