e05e9aaabe282666578558e7bc52888925606307
Dominik Stolz <crazymrcat@gmail.com>
2014-09-27 12:07:50 +0200

    added documentation and changed model
    point1 and  point2 are now point and direction, point2 can be obtained
    with point.add(direction)

--- src/io/github/voidcatz/vectometry/Line.java
+++ src/io/github/voidcatz/vectometry/Line.java
@@ -3,66 +3,111 @@ package io.github.voidcatz.vectometry;
 import io.github.voidcatz.vectometry.util.Angle;
 
 public class Line {
-	public final Vector point1, point2;
+	public final Vector point, direction;
 	
 	public static final Line xAXIS = new Line(new Vector(1, 0));
 	public static final Line yAXIS = new Line(new Vector(0, 1));
 	
-	public Line(Vector point1, Vector point2) {
-		this.point1 = point1;
-		this.point2 = point2;
+	/**
+	 * creates a new line which goes through the given point and has the given direction
+	 * @param point
+	 * @param direction
+	 */
+	public Line(Vector point, Vector direction) {
+		this.point = point;
+		this.direction = direction;
 	}
 	
+	/** 
+	 * creates a new line with the given slope and the y intersect
+	 * @param slope
+	 * @param yIntersect
+	 */
 	public Line(float slope, float yIntersect) {
-		this(new Vector(0, yIntersect), new Vector(0, yIntersect).add(new Vector(1, slope)));
+		this(new Vector(0, yIntersect), new Vector(1, slope));
 	}
 	
-	public Line(Vector vec) {
-		this(Vector.ZERO, vec);
+	/**
+	 * creates a new line which goes through the origin and has the given direction
+	 * @param direction
+	 */
+	public Line(Vector direction) {
+		this(Vector.ZERO, direction);
 	}
 	
-	public Vector vector() {
-		return point2.substract(point1);
-	}
-	
+	/**
+	 * @return the slope of the line
+	 * @see Vector#slope()
+	 */
 	public float slope() {
-		return this.vector().slope();
+		return direction.slope();
 	}
 	
+	/**
+	 * @return the x and y intersects
+	 */
 	public Vector axisIntersects() {
-		float y = this.point1.y - this.slope() * this.point1.x;
+		float y = this.point.y - this.slope() * this.point.x;
 		float x = -y/this.slope();
 		return new Vector(x, y);
 	}
 	
+	/**
+	 * @param other
+	 * @return the intersect between this line and the other line
+	 */
 	public Vector intersect(Line other) { // http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
 		float x = (other.axisIntersects().y - this.axisIntersects().y) / (this.slope() - other.slope());
 		float y = this.slope() * x + this.axisIntersects().y;
 		return new Vector(x, y);
 	}
 	
-	public Angle angle(Line l) {
-		return this.vector().angle(l.vector());
+	/**
+	 * @param other
+	 * @return the angle between this line and the other line
+	 */
+	public Angle angle(Line other) {
+		return this.direction.angle(other.direction);
 	}
 	
-	public boolean contains(Vector vec) {
-		return vec.y == this.slope()*vec.x + this.axisIntersects().y; //TODO: test
+	/**
+	 * @param point
+	 * @return true if this line contains the given point
+	 */
+	public boolean contains(Vector point) {
+		return point.y == this.slope()*point.x + this.axisIntersects().y;
 	}
 	
-	public Vector project(Vector vec) {
-		return point1.add(vec.substract(point1).project(this.vector()));
+	/**
+	 * @param point to be projected
+	 * @return the orthogonal projection of the vector on this line
+	 */
+	public Vector project(Vector point) {
+		return point.add(point.subtract(point).project(this.direction));
 	}
 	
-	public Vector mirror(Vector vec) {
-		return vec.mirror(this.project(vec));
+	/**
+	 * @param point
+	 * @return reflected point
+	 */
+	public Vector reflect(Vector point) {
+		return point.reflect(this.project(point));
 	}
 	
+	/**
+	 * @param point
+	 * @return line which is parallel to this line and goes through the given point
+	 */
 	public Line getParallel(Vector point) {
-		return new Line(point, point.add(this.vector()));
+		return new Line(point, point.add(this.direction));
 	}
 	
+	/**
+	 * @param other
+	 * @return true if the given line is parallel to this line
+	 */
 	public boolean isParallel(Line other) {
-		return this.vector().isParallel(other.vector());
+		return this.direction.isParallel(other.direction);
 	}
 
 }
