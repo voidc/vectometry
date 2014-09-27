e990df22a116aae13d560b6bc67c1ee455a757a2
Dominik Stolz <crazymrcat@gmail.com>
2014-09-27 12:23:33 +0200

    added documentation for Polygon

--- src/io/github/voidcatz/vectometry/Polygon.java
+++ src/io/github/voidcatz/vectometry/Polygon.java
@@ -2,7 +2,6 @@ package io.github.voidcatz.vectometry;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import io.github.voidcatz.vectometry.util.Angle;
@@ -10,11 +9,18 @@ import io.github.voidcatz.vectometry.util.Angle;
 public class Polygon {
 	public final Vector[] vertices;
 	
+	/**
+	 * creates a new polygon with the given vertices
+	 * @param vertices
+	 */
 	public Polygon(Vector[] vertices) {
 		if(vertices.length < 3) throw new InvalidParameterException("A Polygon must at least have 3 vertices");
 		this.vertices = vertices;
 	}
 	
+	/**
+	 * @return an array of all segments between the vertices
+	 */
 	public Segment[] getSegments() {
 		Segment[] segments = new Segment[vertices.length];
 		for(int v = 0; v < vertices.length; v++) {
@@ -23,6 +29,9 @@ public class Polygon {
 		return segments;
 	}
 	
+	/**
+	 * @return the perimeter of this polygon
+	 */
 	public float perimeter() {
 		float sum = 0;
 		for(Segment seg : getSegments()) {
@@ -31,6 +40,9 @@ public class Polygon {
 		return sum;
 	}
 	
+	/**
+	 * @return the area of this polygon
+	 */
 	public float area() {
 		float sum = 0;
 		for(int v = 0; v < vertices.length; v++) {
@@ -39,6 +51,10 @@ public class Polygon {
 		return Math.abs(sum/2);
 	}
 	
+	/**
+	 * @param line
+	 * @return all intersects with the given line
+	 */
 	public Vector[] intersect(Line line) {
 		List<Vector> intersects = new ArrayList<Vector>();
 		for(Segment seg : getSegments()) {
@@ -47,12 +63,16 @@ public class Polygon {
 		return intersects.toArray(new Vector[0]);
 	}
 	
-	public boolean contains(Vector vec) { // http://stackoverflow.com/questions/217578/point-in-polygon-aka-hit-test
+	/**
+	 * @param point
+	 * @return true if this polygon contains the given point
+	 */
+	public boolean contains(Vector point) { // http://stackoverflow.com/questions/217578/point-in-polygon-aka-hit-test
 		boolean c = false;
 		for (int i = 0, j = this.vertices.length - 1; i < this.vertices.length; j = i++) {
-			if (((this.vertices[i].y > vec.y) != (this.vertices[j].y > vec.y))
-					&& (vec.x < (this.vertices[j].x - this.vertices[i].x)
-							* (vec.y - this.vertices[i].y)
+			if (((this.vertices[i].y > point.y) != (this.vertices[j].y > point.y))
+					&& (point.x < (this.vertices[j].x - this.vertices[i].x)
+							* (point.y - this.vertices[i].y)
 							/ (this.vertices[j].y - this.vertices[i].y)
 							+ this.vertices[i].x))
 				c = !c;
@@ -60,15 +80,24 @@ public class Polygon {
 		return c;
 	}
 	
+	/**
+	 * @param scalar
+	 * @param center
+	 * @return scaled polygon
+	 */
 	public Polygon scale(float scalar, Vector center) {
 		Vector[] vtc = new Vector[this.vertices.length];
 		for(int v = 0; v < this.vertices.length; v++) {
-			Vector difference = this.vertices[v].substract(center);
+			Vector difference = this.vertices[v].subtract(center);
 			vtc[v] = center.add(difference.scale(scalar));
 		}
 		return new Polygon(vtc);
 	}
 	
+	/**
+	 * @param tranformation vector by which each vertex is moved
+	 * @return polygon which is moved by the transformation
+	 */
 	public Polygon move(Vector tranformation) {
 		Vector[] vtc = new Vector[this.vertices.length];
 		for(int v = 0; v < this.vertices.length; v++) {
@@ -77,6 +106,11 @@ public class Polygon {
 		return new Polygon(vtc);
 	}
 	
+	/**
+	 * @param angle
+	 * @param center
+	 * @return polygon which is rotated by the angle around the center
+	 */
 	public Polygon rotate(Angle angle, Vector center) {
 		Vector[] vtc = new Vector[this.vertices.length];
 		for(int v = 0; v < this.vertices.length; v++) {
@@ -85,6 +119,9 @@ public class Polygon {
 		return new Polygon(vtc);
 	}
 	
+	/**
+	 * @return the bounding rectangle of this polygon
+	 */
 	public Rectangle getBounds() {
 		float xMin = vertices[0].x;
 		float xMax = vertices[0].x;
@@ -99,14 +136,43 @@ public class Polygon {
 		return new Rectangle(new Vector(xMin, yMin), xMax - xMin, yMax - yMin);
 	}
 	
+	/**
+	 * @return the centroid of this polygon
+	 */
+	public Vector centroid() {
+		return null; //TODO: implement
+	}
+	
+	/**
+	 * @param other
+	 * @return the point on this polygon which is nearest to the given polygon
+	 */
+	public Vector nearestPoint(Vector other) {
+		return null; //TODO: implement
+	}
+	
+	/**
+	 * @param other
+	 * @return unified polygon which consists of this polygon and the other polygon
+	 */
+	public Polygon join(Polygon other) {
+		return null; //TODO: implement
+	}
+	
+	/**
+	 * creates new regular polygon with the given side and the specified number of vertices
+	 * @param side
+	 * @param vertices
+	 * @return regular polygon 
+	 */
 	public static Polygon regularPolygon(Segment side, int vertices) {
 		if(vertices < 3) throw new InvalidParameterException("A Polygon must at least have 3 vertices");
 		Angle angle = Angle.deg((vertices - 1) * 180 / vertices);
 		Vector[] vtc = new Vector[vertices];
-		vtc[0] = side.point1;
-		vtc[1] = side.point2;
+		vtc[0] = side.pointA();
+		vtc[1] = side.pointB();
 		for(int v = 2; v < vertices; v++) {
-			vtc[v] = vtc[v-2].substract(vtc[v-1]).rotate(angle, vtc[v-1]);
+			vtc[v] = vtc[v-2].subtract(vtc[v-1]).rotate(angle, vtc[v-1]);
 		}
 		return new Polygon(vtc);
 	}
