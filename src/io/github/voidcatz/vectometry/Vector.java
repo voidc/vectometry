d481d0908b4f6916f087787c528c450bb365b412
Dominik Stolz <crazymrcat@gmail.com>
2014-10-03 14:49:17 +0200

    added constant directions to Vector

--- src/io/github/voidcatz/vectometry/Vector.java
+++ src/io/github/voidcatz/vectometry/Vector.java
@@ -7,6 +7,10 @@ public class Vector implements Comparable<Vector> {
 	public final float x, y;
 	
 	public static final Vector ZERO = new Vector(0, 0);
+	public static final Vector RIGHT = new Vector(1, 0);
+	public static final Vector UP = new Vector(0, 1);
+	public static final Vector LEFT = new Vector(-1, 0);
+	public static final Vector DOWN = new Vector(0, -0);
 	
 	/**
 	 * @param x coordinate
